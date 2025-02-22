725eb2c09628ee37b604f538ba65db74e6ff57b4
Therese Nevander <thne1900@student.miun.se>
2025-02-22 22:10:11 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+//Javascript function that enables the hamburger menu to be activated.
+"use strict";
+document.getElementById("menu-hamburger").addEventListener("click",function(){
+    const links=document.getElementById("links");
+
+    links.classList.toggle("active");
+
+});
+
